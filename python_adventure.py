af066ca06dc2a9de7d44c28dca27dcd83ea06a61
Noah <noahmldavis@hotmail.com>
2021-12-10 16:59:09 +0000

    added base object class and error handling

--- python_adventure.py
+++ python_adventure.py
@@ -1,28 +1,53 @@
-from typing import Dict
+from typing import Any, Dict
 
 
-class Clue:
+# Errors
+class MissingAttributeError(Exception):
+  pass
+
+class AdventureObject:
   def __init__(self, **data) -> None:
-    self.data = data
-    self.name = data.name
-    self.desc = data.desc
-    self.is_item = data.is_item
+    self._data = data
+    self._required_attrs = []
+    self._validate()
+  
+  def _validate(self) -> None:
+    for attr in self._required_attrs:
+      if not attr in self._data:
+        raise MissingAttributeError(f"Invalid {self.__name__}: missing attribute {attr}")
+
+
+class Clue(AdventureObject):
+  def __init__(self, **data) -> None:
+    self._required_attrs = ["name"]
+    super().__init__(**data)
+
+    # self._required_attrs = ["name", ""]
+
+    # for attr in ["name", "desc", "is_item"]:
+    #   if not attr in self._data:
+    #     self._data[attr] = None
   
   def __hash__(self) -> int:
-    return hash((self.name, self.desc, self.is_item))
+    return hash(self._data)
   
   def __eq__(self, __o: object) -> bool:
-    return (self.name, self.desc, self.is_item) == (__o.name, __o.desc, __o.is_item)
+    return self._data == __o._data
 
-  def __getattr__(self, attr) -> Any:
-    if attr in self.data:
-      return self.data[attr]
-    else:
-      object.__getattr__(attr)
+  # def __getattr__(self, attr) -> Any:
+  #   if attr in self._data:
+  #     return self._data[attr]
+  #   else:
+  #     object.__getattr__(attr)
+  
+  # def __setattr__(self, __name: str, __value: Any) -> None:
+  #   if __name in self._data:
+  #     self._data[__name] = __value
 
 
-# class Room:
-#   def __init__(self, name: str, desc: str, clues: Dict={}) -> None:
-#     self.name = name
-#     self.desc = desc
-#     self.clues = clues.copy()
+class Room:
+  def __init__(self, **data) -> None:
+    self._data = data
+
+
+test_clue = Clue()
