7a6f726a0b5e6370d8e02f58e1942fd4dea8a428
Noah <noahmldavis@hotmail.com>
2021-12-10 15:57:37 +0000

    added getattr method for clue class

--- python_adventure.py
+++ python_adventure.py
@@ -3,6 +3,7 @@ from typing import Dict
 
 class Clue:
   def __init__(self, **data) -> None:
+    self.data = data
     self.name = data.name
     self.desc = data.desc
     self.is_item = data.is_item
@@ -13,9 +14,15 @@ class Clue:
   def __eq__(self, __o: object) -> bool:
     return (self.name, self.desc, self.is_item) == (__o.name, __o.desc, __o.is_item)
 
+  def __getattr__(self, attr) -> Any:
+    if attr in self.data:
+      return self.data[attr]
+    else:
+      object.__getattr__(attr)
 
-class Room:
-  def __init__(self, name: str, desc: str, clues: Dict={}) -> None:
-    self.name = name
-    self.desc = desc
-    self.clues = clues.copy()
+
+# class Room:
+#   def __init__(self, name: str, desc: str, clues: Dict={}) -> None:
+#     self.name = name
+#     self.desc = desc
+#     self.clues = clues.copy()
