54279b04ea8e1c6f8dee802d3419880a3f041905
Noah <noahmldavis@hotmail.com>
2021-12-13 15:51:46 +0000

    fixed dict issues, added set/get in base class

--- python_adventure.py
+++ python_adventure.py
@@ -5,13 +5,11 @@ import re
 class ConstructionError(Exception):
   pass
 
-# Parent Class Definition
+# AdventureObject definition (Base Class)
 class AdventureObject:
   def __init__(self, **data) -> None:
     self._data = data.copy()
     self._validate()
-
-    self.is_default = False
   
   # def __hash__(self) -> int:
   #   return hash(self.name)
@@ -19,9 +17,17 @@ class AdventureObject:
   # def __eq__(self, __o: object) -> bool:
   #   return self._data == __o._data
   
-  def __str__(self) -> str:
-    return "\n" + self._prefix + " " + self.get("name")
+  # def __str__(self) -> str:
+  #   return "\n" + self._prefix + " " + self.get("name")
+    
+  # def describe(self, detailed=False) -> None:
+  #   print()
+  #   print(self.get("prefix") + self.get("name"))
+  #   if detailed: print(self.get("desc"))
   
+  def __repr__(self) -> str:
+    return str(self._data)
+
   # Validates the object against its own required attributes
   def _validate(self) -> None:
     if hasattr(self, "_required_attrs"):
@@ -33,24 +39,23 @@ class AdventureObject:
           if typ != attr[1]:
             raise ConstructionError(f"Invalid type for \"{attr[0]}\" attribute in {type(self).__name__} object (expected {str(attr[1].__name__)}, got {str(typ.__name__)})")
     
-    if not hasattr(self, "_prefix"):
-      self._prefix = ""
+    if not hasattr(self, "prefix"):
+      self.set("prefix", "")
   
-  def default(self):
-    self.is_default = True
-    return self
-
-  def describe(self) -> None:
-    print(self.get("desc"))
-  
+  # setters and getters
   def get(self, attr) -> Any:
     return self._data[attr]
   
+  def set(self, attr, value) -> None:
+    self._data[attr] = value
+  
+  # other base functions
+
   def id(self) -> str:
     return self.get("name").lower()
   
   def rename(self, name: str) -> None:
-    self._data["name"] = name
+    self.set("name", name)
 
 
 class Clue(AdventureObject):
@@ -60,18 +65,38 @@ class Clue(AdventureObject):
       ("desc", str),
       ("is_item", bool)
     ]
-    self._prefix = "  ~"
 
     super().__init__(**data)
+
+    self.set("prefix", "~ ")
+    self.set("actions", {})
   
-  def __str__(self) -> str:
-    return self._prefix + " " + self.get("name")
+  # def __str__(self) -> str:
+  #   return self._prefix + " " + self.get("name")
 
   def cmd(self, name: str, arg):
     def check(arg=None) -> str:
       return self.get("desc")
     
+    if name not in locals():
+      return self.get("actions")[name](arg)
+
     return locals()[name](arg)
+  
+  def action(self, cmd_name, *cmd_action: tuple):
+    exec(f"""def {cmd_name}(self):""")
+
+    new_action = locals()[cmd_name]
+
+    return new_action
+  
+  def on(self, cmd_name, cmd_action):
+    if cmd_name in self.get("actions"):
+      print(f"Action '{cmd_name}' already defined.")
+    else:
+      self.get("actions")[cmd_name] = self.action(cmd_name, cmd_action)
+    
+    return self
 
 
 class Room(AdventureObject):
@@ -81,49 +106,60 @@ class Room(AdventureObject):
       ("desc", str),
       ("clues", dict)
     ]
-    self._prefix = " *"
-
-    self._open = False
-    self._unlocked = False
 
     super().__init__(**data)
+
+    self.set("prefix", "* ")
+
+    self.set("is_default", False)
+    self.set("is_open", False)
+    self.set("is_locked", False)
   
   def __getitem__(self, key: str) -> Clue:
-    for clue in self.get("clues"):
-      if clue.id() == key: return clue
-    raise KeyError(key)
+    return self.get("clues")[key]
   
   def __iter__(self):
-    return iter(self.get("clues"))
+    return iter(self.get("clues").values())
   
-  def __str__(self) -> str:
-    if self._open:
-      return super().__str__() + "\n" + "\n".join([str(clue) for clue in self])
-    else:
-      return super().__str__()
+  # def __str__(self) -> str:
+  #   if self._open:
+  #     return super().__str__() + "\n" + "\n".join([str(clue) for clue in self])
+  #   else:
+  #     return super().__str__()
   
+  def default(self):
+    self.set("is_default", True)
+    return self
+  
+  # def describe(self, detailed=False) -> None:
+  #   super().describe(detailed)
+
+  #   for clue in self:
+  #     print(clue.get("name"))
+
   def push(self, clue: Clue) -> Clue:
     self.get("clues").add(clue)
     return clue
 
   def pop(self, key: str) -> None:
-    for clue in self.get("clues").copy():
-      if clue.get("name") == key:
-        self.get("clues").remove(clue)
-        return clue
+    return self.get("clues").pop(key, None)
   
   # Unopened rooms will not show their clues in the console display
   def open(self):
-    self._open = True
+    self.set("is_open", True)
     return self
 
   def close(self):
-    self._open = False
+    self.set("is_open", False)
     return self
   
   # Locked rooms cannot be opened until unlocked
+  def lock(self):
+    self.set("is_locked", True)
+    return self
+
   def unlock(self):
-    self._unlocked = True
+    self.set("is_locked", False)
     return self
 
 
@@ -134,23 +170,36 @@ class Location(AdventureObject):
       ("desc", str),
       ("rooms", dict)
     ]
-    self._prefix = "**"
+
     super().__init__(**data)
+
+    self.set("prefix", "** ")
+    self.set("is_default", False)
   
   def __getitem__(self, key: str) -> Room:
-    for room in self.get("rooms"):
-      if room.id() == key: return room
-    raise KeyError(key)
+    return self.get("rooms")[key]
   
   def __iter__(self):
-    return iter(self.get("rooms"))
+    return iter(self.get("rooms").values())
   
-  def __str__(self) -> str:
-    return super().__str__() + "\n" + "\n".join([str(room) for room in self]) + "\n"
+  # def __str__(self) -> str:
+  #   return super().__str__() + "\n" + "\n".join([str(room) for room in self]) + "\n"
+  
+  def default(self):
+    self.set("is_default", True)
+    return self
   
   def get_default(self) -> Room:
-    for room in self.get("rooms"):
-      if room.is_default: return room
+    for room in self:
+      if room.get("is_default"): return room
+
+  # def describe(self, detailed=False) -> None:
+  #   super().describe(detailed)
+
+  #   for room in self:
+  #     room.describe()
+    
+  #   print()
 
 
 class TextAdventure(AdventureObject):
@@ -162,9 +211,9 @@ class TextAdventure(AdventureObject):
 
     super().__init__(**data)
 
-    self._inventory = {}
-    self._current_location = self.get_default().id()
-    self._current_room = self[self._current_location].get_default().id()
+    self.set("inventory", {})
+    self.set_default_location()
+    self.set_default_room()
   
   def __call__(self) -> Any:
     player_cmd = self._parse_cmd(input("\n>> "))
@@ -176,15 +225,13 @@ class TextAdventure(AdventureObject):
     return self()
 
   def __getitem__(self, key: str) -> Location:
-    for location in self.get("locations"):
-      if location.id() == key: return location
-    raise KeyError(key)
+    return self.get("locations")[key]
   
   def __iter__(self):
-    return iter(self.get("locations"))
+    return iter(self.get("locations").values())
   
-  def __str__(self) -> str:
-    return "\n".join([str(location) for location in self]) + "\n"
+  # def __str__(self) -> str:
+  #   return "\n".join([str(location) for location in self]) + "\n"
 
   # Parses a command and its argument into dict from given input text
   def _parse_cmd(self, cmd_str: str) -> dict:
@@ -198,32 +245,41 @@ class TextAdventure(AdventureObject):
 
   def cmd(self, name: str, arg):
     def go(location_name: str) -> None:
-      self._current_location = location_name
+      self.set("current_location", self[location_name])
+      self.set_default_room()
       print(f"Going to {location_name}...")
     
     def enter(room_name: str) -> None:
-      current_location = self[self._current_location]
-
-      current_location[self._current_room].close()
-      self._current_room = room_name
-      current_location[self._current_room].open()
+      location_name = self.get("current_location").id()
+      self.get("current_room").close()
+      self.set("current_room", self[location_name][room_name])
+      self.get("current_room").open()
 
       print(f"Entering {room_name}...")
 
     def take(clue_name: str) -> None:
-      current_room = self[self._current_location][self._current_room]
-      if current_room[clue_name].get("is_item"):
-        self._inventory.append(current_room.pop(clue_name))
+      room = self.get("current_room")
+      if room[clue_name].get("is_item"):
+        self.get("inventory")[clue_name] = room.pop(clue_name)
     
     if name in locals():
       return locals()[name](arg)
     else:
       return None
-
   
-  def get_default(self) -> Location:
-    for location in self.get("locations"):
-      if location.is_default: return location
+  def get_default_location(self) -> Location:
+    for location in self:
+      if location.get("is_default"): return location
+  
+  def get_default_room(self) -> Room:
+    for room in self.get("current_location"):
+      if room.get("is_default"): return room
+
+  def set_default_location(self) -> None:
+    self.set("current_location", self.get_default_location())
+  
+  def set_default_room(self) -> None:
+    self.set("current_room", self.get_default_room())
 
 
 # clues
@@ -247,6 +303,16 @@ cl_phone = Clue(
   desc="It's unplugged.",
   is_item=True
 )
+cl_dumpster = Clue(
+  name="Dumpster",
+  desc="For some reason, it's locked.",
+  is_item=False
+)
+cl_snowglobe = Clue(
+  name="Snowglobe",
+  desc="\'For Melinda\' is engraved on its plaque.",
+  is_item=True
+)
 
 # rooms
 rm_hallway = Room(
@@ -265,24 +331,37 @@ rm_bedroom = Room(
     cl_stained_wall.id(): cl_stained_wall
   }
 )
+rm_dark_alley = Room(
+  name="Dark Alley",
+  desc="Well lit, despite the name.",
+  clues={
+    cl_dumpster.id(): cl_dumpster,
+    cl_snowglobe.id(): cl_snowglobe
+  }
+)
 
 # locations
 ln_house = Location(
   name="House",
   desc="Smells like a house.",
   rooms={
-    rm_hallway.id(): rm_hallway.unlock().default(),
-    rm_bedroom.id(): rm_bedroom.unlock()
+    rm_hallway.id(): rm_hallway.default(),
+    rm_bedroom.id(): rm_bedroom.lock()
+  }
+)
+ln_street = Location(
+  name="Street",
+  desc="Looks dark, but it's midday.",
+  rooms={
+    rm_dark_alley.id(): rm_dark_alley.default()
   }
 )
 
+# text adventure
 test_adv = TextAdventure(
   title="TITLE",
   locations={
-    ln_house.id(): ln_house.default()
+    ln_house.id(): ln_house.default(),
+    ln_street.id(): ln_street
   }
 )
-
-print(test_adv)
-# test_adv.cmd("take", "phone")
-# print(test_adv)
